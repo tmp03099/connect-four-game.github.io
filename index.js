5e763d2ea1209adad25701e32e4dcb2aac57e19c
My Phung Tieu <tmp03099@gmail.com>
2023-02-27 19:11:06 -0500

    updated

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const body = document.querySelector('body');
 const girdboardGame = document.getElementById('grid-board-game');
 const board = document.querySelectorAll('board');
 
+//create board game
+//For each row create 7 columns to get the match column id
+function boardGame(){
+    for (let r = 0; r < 6; r++){
+        for(let c = 0; c < 7; c++){
+            const div = document.createElement('div');
+            div.classList.add('board')
+            div.classList.add(`row-${r}`);
+            div.classList.add(`col-${c}`);
+            div.id = `${r}${c}` 
+            girdboardGame.appendChild(div);
+        }
+    }
+}
+
 
 //create top board game to user control
 function topBoardGame(){
@@ -26,64 +41,13 @@ function topBoardGame(){
     }
 }
 
-//create board game
-//For each row create 7 columns to get the match column id
-function boardGame(){
-    for (let r = 0; r < 6; r++){
-        for(let c = 0; c < 7; c++){
-            const div = document.createElement('div');
-            div.classList.add('board')
-            div.classList.add(`row-${r}`);
-            div.classList.add(`col-${c}`);
-            div.id = `${r}${c}` 
-            girdboardGame.appendChild(div);
-        }
-    }
-}
-
 
 //call function
 body.appendChild(girdboardGame);
 topBoardGame();
 boardGame();
 
-
-/* create current piece function to show color piece depend on players
-    add btnID as parameter to get which col will be selected
-    then show the current piece
-*/
-function putPiece (btnID){
-
-    //select query button from buttonID parameter
-    const selectedBtn = document.getElementById(btnID);
-
-    //get the last number in the id of getBtnID object
-    const lastID = selectedBtn.id.slice(-1);
-    
-    const colSelected = document.querySelectorAll(`.col-${lastID}`);
-    console.log(`%c--col${lastID}--`,`color:yellow`);
-    console.log(colSelected)
-
-    for (let c = colSelected.length - 1; c >= 0 ; c--){
-
-        if (colSelected[c].classList.contains('red') || colSelected[c].classList.contains('yellow') ){
-            console.log(`slot ${c} filled`)
-        }else{
-            colSelected[c].classList.add(currentPlayer);
-            break;
-        }
-    }
-
-    if (currentPlayer === player1){
-        currentPlayer = player2;
-    }else{
-        currentPlayer = player1;
-    }
-
-}
-
-
-//Find which button will be selected 
+//Find which selected button
 const buttons = document.querySelectorAll('.select-button');
 
 buttons.forEach(button =>{
@@ -93,8 +57,50 @@ buttons.forEach(button =>{
         //When clicked the current piece will show on the board game
         //call function currentPiece 
         putPiece(event.target.id);
-        console.log(event.target.id);
         
-
+        
     })
 })
+
+
+/* 
+putPiece function to show color piece depend on players
+Input: btnID  - as parameter to get which col will be selected
+then show the current piece
+*/
+function putPiece (btnID){
+
+    //select query button from buttonID parameter
+    const selectedBtn = document.getElementById(btnID);
+    console.log(`%c--button selected--`,`color:yellow`);
+    console.log(selectedBtn.id);
+
+    //get the last character in the id of selectedBtn.  This is the column number 
+    const colNum = selectedBtn.id.slice(-1);
+    
+    //get all spots for column number
+    const colSelected = document.querySelectorAll(`.col-${colNum}`);
+    console.log(`%c--col${colNum}--`,`color:yellow`);
+    console.log(colSelected)
+
+    //loop from botton to top to check where to put the piece
+    for (let c = colSelected.length - 1; c >= 0 ; c--){
+
+        if (colSelected[c].classList.contains('red') || colSelected[c].classList.contains('yellow') ){
+            console.log(`slot ${c} filled`)
+
+        }else{
+            colSelected[c].classList.add(currentPlayer);
+            if (currentPlayer === player1){
+                currentPlayer = player2;
+            }else{
+                currentPlayer = player1;
+            }
+            break;
+        }
+    }
+
+
+}
+
+
