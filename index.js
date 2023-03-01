204776578846e5f7e16541abb3917e5e4dc4fa22
My Phung Tieu <tmp03099@gmail.com>
2023-02-28 20:49:54 -0500

    changed while loop to find begining of diagonal

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ boardGame();
 //Find which selected button
 const buttons = document.querySelectorAll('.select-button');
 
+//loop button and addEventListener for click event 
 buttons.forEach(button =>{
 
     button.addEventListener('click', function(event){
@@ -66,12 +67,12 @@ buttons.forEach(button =>{
 
 /* 
 putPiece function to show color piece depend on players
-Input: btnID  - as parameter to get which col will be selected
+Input: btnID  - as input parameter to get which col will be selected
 then show the current piece
 */
 function putPiece (btnID){
 
-    //select query button from buttonID parameter
+    //select element by ID with buttonID parameter
     const selectedBtn = document.getElementById(btnID);
     console.log(`%c--button selected--`,`color:yellow`);
     console.log(selectedBtn.id);
@@ -95,7 +96,7 @@ function putPiece (btnID){
         }else{
             colSelected[r].classList.add(currentPlayer);
             console.log(currentPlayer,"orgin");
-            //call the checkWinner function before check the current player and change it
+            //call the checkWinner function to check winner and change the currentplayer
             checkWinner(colNum, r);
             if (currentPlayer === player1){
                 currentPlayer = player2;
@@ -109,11 +110,18 @@ function putPiece (btnID){
 }
 
 let total = 0;
-//Check winner
+/*
+    Check winner function to check who is winner
+    Input: colNum as a column number of current piece (last played piece)
+           rolNum as a row number of current piece
+*/
 function checkWinner(colNum, rolNum){
 
     //vertical
+    //get list element from the column number 
     const allColumns = document.querySelectorAll(`.col-${colNum}`);
+
+    //loop each element to check 4 pieces are matching color is win
     for( let i = allColumns.length - 1  ; i >= 0; i --){
         if(allColumns[i].classList.contains(currentPlayer)){
             total += 1;
@@ -128,10 +136,13 @@ function checkWinner(colNum, rolNum){
     }
 
     //horizontal
+    //get list element from the row number
     const allRows = document.querySelectorAll(`.row-${rolNum}`); // c is row number
     console.log(allRows)
+
+    //loop each element to check 4 peices are matching color is win
     for (let r = 0; r < allRows.length ; r++){
-        if(allRows[r].classList.contains('red')){
+        if(allRows[r].classList.contains(currentPlayer)){
             total += 1;
             if (total === 4){
                 setWinner();
@@ -143,16 +154,21 @@ function checkWinner(colNum, rolNum){
     }
 
     //diagonal
-    let r = rolNum;
-    let c = Number(colNum);
-    for( let i = 0; i< allColumns.length; i++){
+    let r = rolNum; //5
+    let c = Number(colNum); // 0
+    while (r < 5 && c > 0){
+        r = r + 1;
+        c = c - 1;
+        console.log(r,c);
+    }
+    console.log("pass", r,c)
+
+    while(r > 0){
         r = r - 1;
         c = c + 1;
         console.log(r, c);
         const diagonal = document.querySelector(`.row-${r}.col-${c}`);
         console.log(diagonal);
-        
-
     }
 
 }
